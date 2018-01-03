9b3cfd92d6405e4ef1ad5f0c40e0300175af4914
Elise Barnes <barnese3@go.stockton.edu>
2018-01-03 12:36:49 -0500

    first commit

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,53 @@
+// shows certain elements when you scroll over them
+$(document).ready(function() {
+    
+    // Every time the window is scrolled ...
+    $(window).scroll( function(){
+    
+        // Check the location of each desired element
+        $('.scroll-show').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            // If the object is completely visible in the window, fade it in
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},3500);
+            }
+        }); 
+    });
+});
+
+// fade-in each skill on about me page at a different speed on scroll
+var speeds = [700, 1700, 2700, 3700];
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.scroll-show-skills').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_object ){
+                $(this).fadeIn(speeds[i]);
+            }
+        }); 
+    });
+});
+
+//fade-in portfolio elements at different speeds
+var speeds = [1000, 2000, 3000];
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.scroll-show-portfolio').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},speeds[i]);
+            }
+        }); 
+    });
+});
+
+console.log("Do you come here often?");
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get '/about-me' => 'pages#about'
+  get '/portfolio' => 'pages#portfolio'
+  get '/contact' => 'pages#contact'
+end
