71bce71b79dca93f74522f0522ed240259631329
Elise Barnes <elisebarnes@Elises-MBP.home>
2019-02-22 12:00:20 -0500

    full portfolio update

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
         $('.scroll-show-portfolio').each( function(i){
             
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 350;
             
             if( bottom_of_window > bottom_of_object ){
                 $(this).animate({'opacity':'1'},speeds[i]);
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
-  get '/about-me' => 'pages#about'
+  get '/about' => 'pages#about'
   get '/portfolio' => 'pages#portfolio'
   get '/contact' => 'pages#contact'
-  get '/whereto' => 'pages#whereto'
-  get '/friendlybean' => 'pages#friendlybean'
-  get '/plenty' => 'pages#plenty'
-  get '/crystal-cove' => 'pages#crystal-cove'
+  get '/services-and-prices' => 'pages#services-and-prices'
+  # get '/friendlybean' => 'pages#friendlybean'
+  # get '/plenty' => 'pages#plenty'
+  # get '/crystal-cove' => 'pages#crystal-cove'
 end
